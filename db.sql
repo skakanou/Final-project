a5143c31c09e3d22136e8182ef333fad2d324c35
Karixk Nounou <80161785+skakanou@users.noreply.github.com>
2026-06-22 18:58:00 -0400

    Add files via upload

--- db.sql
+++ db.sql
@@ -16,90 +16,121 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `bookings`
+-- Table structure for table `alembic_version`
 --
 
-DROP TABLE IF EXISTS `bookings`;
+DROP TABLE IF EXISTS `alembic_version`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `bookings` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary key',
-  `number_tickets` int NOT NULL,
-  `status` enum('approved','cancel','on hold') DEFAULT 'on hold',
-  `user_id` int NOT NULL,
-  `event_id` int NOT NULL,
-  `booking_date` date NOT NULL,
-  PRIMARY KEY (`id`),
-  KEY `user_id` (`user_id`),
-  KEY `event_id` (`event_id`),
-  CONSTRAINT `bookings_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
-  CONSTRAINT `bookings_ibfk_2` FOREIGN KEY (`event_id`) REFERENCES `events` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Reservation management';
+CREATE TABLE `alembic_version` (
+  `version_num` varchar(32) NOT NULL,
+  PRIMARY KEY (`version_num`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `bookings`
+-- Dumping data for table `alembic_version`
 --
 
-LOCK TABLES `bookings` WRITE;
-/*!40000 ALTER TABLE `bookings` DISABLE KEYS */;
-/*!40000 ALTER TABLE `bookings` ENABLE KEYS */;
+LOCK TABLES `alembic_version` WRITE;
+/*!40000 ALTER TABLE `alembic_version` DISABLE KEYS */;
+INSERT INTO `alembic_version` VALUES ('21f9b6bd1305');
+/*!40000 ALTER TABLE `alembic_version` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `events`
+-- Table structure for table `booking`
 --
 
-DROP TABLE IF EXISTS `events`;
+DROP TABLE IF EXISTS `booking`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `events` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+CREATE TABLE `booking` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `booking_date` date NOT NULL,
+  `event_id` int NOT NULL,
+  `created_at` datetime DEFAULT NULL,
+  `state` enum('Confirmed','Cancelled') NOT NULL DEFAULT 'Confirmed',
+  `quantity` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `event_id` (`event_id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `booking_ibfk_1` FOREIGN KEY (`event_id`) REFERENCES `event` (`id`),
+  CONSTRAINT `booking_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `user` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=12 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `booking`
+--
+
+LOCK TABLES `booking` WRITE;
+/*!40000 ALTER TABLE `booking` DISABLE KEYS */;
+INSERT INTO `booking` VALUES (2,3,'2026-06-20',3,'2026-06-21 02:01:06','Cancelled',0),(3,3,'2026-06-21',3,'2026-06-21 15:29:51','Cancelled',0),(4,3,'2026-06-21',6,'2026-06-21 17:56:52','Cancelled',0),(6,3,'2026-06-21',5,'2026-06-21 18:59:09','Cancelled',0),(11,3,'2026-06-22',3,'2026-06-22 22:50:12','Cancelled',91);
+/*!40000 ALTER TABLE `booking` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `event`
+--
+
+DROP TABLE IF EXISTS `event`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `event` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `event_name` varchar(255) NOT NULL,
   `event_date` date NOT NULL,
   `location` varchar(255) NOT NULL,
   `seats_available` int NOT NULL,
-  `categorie` varchar(255) NOT NULL,
-  `description` text NOT NULL,
+  `description` text,
+  `created_at` datetime DEFAULT NULL,
+  `category` varchar(50) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Event management';
+) ENGINE=InnoDB AUTO_INCREMENT=8 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `events`
+-- Dumping data for table `event`
 --
 
-LOCK TABLES `events` WRITE;
-/*!40000 ALTER TABLE `events` DISABLE KEYS */;
-/*!40000 ALTER TABLE `events` ENABLE KEYS */;
+LOCK TABLES `event` WRITE;
+/*!40000 ALTER TABLE `event` DISABLE KEYS */;
+INSERT INTO `event` VALUES (3,'Demain tout commence','2026-06-24','Douala Bercy',99,'Demain c\'est aujourd\'hui et aujourd\'hui c\'etait hier','2026-06-20 13:24:12','Culture'),(4,'Manque d\'inspi','2026-07-17','Babana City',110,'Werrrrrr','2026-06-20 13:24:55','Formation'),(5,'Un dernier pour la route','2026-09-24','Ottawa',99,'Loren Ipsum','2026-06-20 13:25:33','Sport'),(6,'God of War','2026-08-27','Montreal',24,'Viens jouer','2026-06-20 13:26:10','Formation');
+/*!40000 ALTER TABLE `event` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `users`
+-- Table structure for table `user`
 --
 
-DROP TABLE IF EXISTS `users`;
+DROP TABLE IF EXISTS `user`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `users` (
-  `id` int NOT NULL AUTO_INCREMENT COMMENT 'Primary Key',
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `role` enum('client','admin') DEFAULT 'client',
+  `created_at` datetime DEFAULT NULL,
+  `role` varchar(20) DEFAULT NULL,
+  `failed_attempts` int DEFAULT NULL,
+  `locked_until` datetime DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `username` (`username`),
-  UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci COMMENT='Table of users management, default customers';
+  UNIQUE KEY `email` (`email`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `users`
+-- Dumping data for table `user`
 --
 
-LOCK TABLES `users` WRITE;
-/*!40000 ALTER TABLE `users` DISABLE KEYS */;
-/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+LOCK TABLES `user` WRITE;
+/*!40000 ALTER TABLE `user` DISABLE KEYS */;
+INSERT INTO `user` VALUES (1,'patrick','pskakanou@gmail.com','scrypt:32768:8:1$eUOXis3NiU94U57X$4a33095ffffeeb9113b6b5fe66e6c46e663e31f7400c5a37be29c307e0672752c9b0182d82c69fd6b5f04b123a937d285ea1c5c763c8b2b25f009978e76b8007','2026-06-19 17:55:14','client',0,NULL),(2,'admin','admin@admin.com','scrypt:32768:8:1$MOHollZLRMFgCD0u$974eb1c4a4ab65474aa4288d0047f9e09efcbe4614de098a67a01b644b289757f166c7eb302f96ca406183117757c9d9956c3b1f79751222e06ddb3c5837b768','2026-06-19 19:00:55','admin',0,NULL),(3,'eddy','eddy@test.com','scrypt:32768:8:1$bLM6ZFyg11lh9but$1a46aee23dc757ac0d3ca3eb293ab09e15147bcd0d379f7a6f04c07b15e757ffa915e72d4da80f026aeca014e72b057ec33ff6740ffb6b1f26703cde2fde7e97','2026-06-19 23:30:01','client',0,NULL);
+/*!40000 ALTER TABLE `user` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -111,4 +142,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-06-16 20:35:14
+-- Dump completed on 2026-06-22 18:57:30
